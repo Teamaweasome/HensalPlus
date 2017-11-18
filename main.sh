788304f3ca49e0f16766e9f42afd7f9ace289243
Strudy <proteximportsexports@gmail.com>
2017-11-18 15:16:13 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,18 +1,1 @@
-λ 23+0
-23:: Num a => a
-λ primes = filterPrime [2..] 
-  where filterPrime (p:xs) = 
-          p : filterPrime [x | x <- xs, x `mod` p /= 0]
-<hint>:1:8: parse error on input ‘=’
-λ help
-λ 5 + 7
-12:: Num a => a
-λ 12
-12:: Num a => a
-λ "chris"
-"chris":: [Char]
-λ [42,13,22]
-[42,13,22]:: Num t => [t]
-λ sort [42,13,22]
-[13,22,42]:: (Num a, Ord a) => [a]
-λ  
+23+0
